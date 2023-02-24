1bee480c833ad4becff58234b361aff9bc028641
cloudsky1221 <110422625+cloudsky1221@users.noreply.github.com>
2023-02-24 23:45:13 +0530

    update

--- portfolio/src/Api/Weather/Weather.jsx
+++ portfolio/src/Api/Weather/Weather.jsx
@@ -1,11 +1,21 @@
 import "./Weather.css"
 
+import Clear from "./Icons/day-clear.png";
+import Fog from "./Icons/fog.png";
+import FreezingRain from "./Icons/freezing-rain.png";
+import HeavySnow from "./Icons/heavy-snow.png"
+import PartlyCloudy from "./Icons/partly-cloudy.png";
+import Rain from "./Icons/rain.png";
+import Snowfall from "./Icons/snowfall.png";
+import Thunderstorm from "./Icons/thunderstorm.png";
+
 import { useState, useEffect } from "react"
 
 const Weather = () => {
     const [data, setData] = useState();
     const [timer, setTimer] = useState("");
-    const [mood, setMood] = useState(true);
+    // const [mood, setMood] = useState(true);
+    const [icon, setIcon] = useState({title:"Not Known", img:Clear});
 
     // setInterval(() => {
     //     if (mood) setMood(false);
@@ -13,22 +23,93 @@ const Weather = () => {
     //     setTimer("")
     // }, 60000);
 
+    function icons(codeNumber){
+        switch(codeNumber){
+            case 0:
+                setIcon({title:"Clear", img:Clear});
+                break;
+
+            case 1:
+            case 2:
+            case 3:
+                setIcon({title:"Partly Cloudy", img:PartlyCloudy});
+                    break;
+
+            case 45:
+            case 48:
+                setIcon({title:"Fog", img:Fog});
+                break;
+
+            case 51:
+            case 53:
+            case 55:
+                setIcon({title:"Moderate Drizzle", img:Rain});
+                    break;
+
+            case 61:
+            case 63:
+            case 65:
+                setIcon({title:"Freezing Drizzle", img:Rain});
+                break;
+
+            case 66:
+            case 67:
+                setIcon({title:"Freezing Rain", img:FreezingRain});
+                break;
+
+            case 71:
+            case 73:
+            case 75:
+                setIcon({title:"Snowfall", img:Snowfall});
+                    break;
+
+            case 77:
+                setIcon({title:"Snow Grains", img:Snowfall});
+                    break;
+
+            case 80: 
+            case 81: 
+            case 82:
+                setIcon({title:"Rain Showers", img:Rain});
+                    break;
+
+            case 85:
+            case 86:
+                setIcon({title:"Snow Showers", img:Snowfall});
+                    break;
+
+            case 95:
+            case 96:
+            case 99:
+                setIcon({title:"Thunderstorm", img:Thunderstorm});
+                    break;
+
+            default:
+                setIcon({title:"Not Known", img:Clear});
+
+        }
+    }
+
+    setInterval(() => {
+        setTimer("")
+    }, 3600000);
+
     async function kall() {
         await fetch("https://api.open-meteo.com/v1/forecast?latitude=18.52&longitude=73.86&current_weather=true")
-        .then(res => res.json()).then(res => setData(res.current_weather))
+        .then(res => res.json()).then(res => {setData(res.current_weather);icons(res.current_weather.weathercode)})
     }
 
     useEffect(() => {
-        setTimeout(() => {
-            kall()  
-            setTimer("")
-        }, 3600000);
+        kall()
     },[timer])
 
     return (
         <div className="weather-wrapper">
-            {console.count("timer")}
             <div className="city">Pune</div>
+            <div className="icon">
+                <img src={icon.img} alt="icon-image" />
+                <div className="text">{icon.title}</div>
+            </div>
             <div className="temp">Temperature: {data?.temperature}</div>
             <div className="windspeed">Wind-speed: {data?.windspeed}</div>
             <div className="winddirection">Wind-direction: {data?.winddirection}</div>
--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import Widget from './widgets/Widgets';
 import Taskbar from './taskbar/Taskbar';
 import Glass from './glass/Glass';
-import GitHub from './icons/github.png';
-import PDF from './icons/pdf.png';
-import Excel from './icons/excel.png';
+import GitHub from './widgets/icons/github.png';
+import PDF from './widgets/icons/pdf.png';
+import Excel from './widgets/icons/excel.png';
 import Weather from './Api/Weather/Weather';
 
 import './App.css'
@@ -15,6 +15,7 @@ function App() {
 
   return (
     <div className="App">
+      {console.count("time")}
       <Widget name="Download Resume" img={PDF} url="" file=""/>
       <Widget name="Github Profile" img={GitHub} url="https://github.com/cloudsky1221"/>
       <Widget name="Online Excel" img={Excel} url="" />
